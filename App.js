0fe4d2ddf8fe91c25bac25dacdd5e87178a0930d
Alireza Joz <alireza00joz@gmail.com>
2024-02-06 03:00:35 +0330

    set state login

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Form from './ls-react/src/Form/Form'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Form />
+            </div>
+        )
+    }
+}
